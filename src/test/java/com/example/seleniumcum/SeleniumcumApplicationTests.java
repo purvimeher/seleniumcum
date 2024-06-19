a51f73fff58d63b8a00d8a0f2f3e7e3ed75f765c
meherm <meherpurvi2815@gmail.com>
2024-06-19 20:03:36 +0100

    Initial commit

---
+++ src/test/java/com/example/seleniumcum/SeleniumcumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seleniumcum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumcumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
